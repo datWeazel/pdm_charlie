e304f70e588e2fa41622a12d431aa9cb67eec199
Christian <Christian@CHRISTIANPC>
2019-10-16 16:39:21 +0200

    Updated train map

---
+++ PDM Charlie/Assets/Scenes/Stages/Train/AddForce.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForce : MonoBehaviour
+{
+    public float x = 10f;
+    public float y = 10f;
+    public float z = 10f;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(x,y,z,ForceMode.Force);
+    }
+}
+
---
+++ PDM Charlie/Assets/Scenes/Stages/Train/move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+
+    public float speed = -10f;
+    //private Rigidbody rigidbody;
+
+    private void Start()
+    {
+        //this.rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        //rigidbody.MovePosition(new Vector3(speed * Time.deltaTime, 0, 0));
+        
+    }
+}
