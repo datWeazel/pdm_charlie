3afe8fc27c74e27ea26344ba7bc9dee352bfb838
Christian <Christian@CHRISTIANPC>
2019-10-18 16:08:46 +0200

    Train map updated

--- PDM Charlie/Assets/Scenes/Stages/Train/move.cs
+++ PDM Charlie/Assets/Scenes/Stages/Train/move.cs
@@ -6,11 +6,15 @@ public class move : MonoBehaviour
 {
 
     public float speed = -10f;
+    public float resetDistance = 9000f;
+
+    private Vector3 startPos;
     //private Rigidbody rigidbody;
 
     private void Start()
     {
         //this.rigidbody = GetComponent<Rigidbody>();
+        startPos = transform.position;
     }
 
     // Update is called once per frame
@@ -19,5 +23,9 @@ public class move : MonoBehaviour
         transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
         //rigidbody.MovePosition(new Vector3(speed * Time.deltaTime, 0, 0));
         
+        if(Vector3.Distance(startPos, transform.position) > resetDistance)
+        {
+            transform.position = startPos;
+        }
     }
 }
