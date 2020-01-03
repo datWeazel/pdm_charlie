b70dc546f1a550436c0c7ca0bbef7e584d8f7c22
datWeazel <kagayushi@hotmail.de>
2020-01-03 17:31:23 +0100

    Added death explosion sound for bottom deathzones

--- PDM Charlie/Assets/Scripts/GameLogic/DeathZonesController.cs
+++ PDM Charlie/Assets/Scripts/GameLogic/DeathZonesController.cs
@@ -7,12 +7,28 @@ public class DeathZonesController : MonoBehaviour
     const float cameraShakeDuration = 0.5f;
     const float cameraShakeStrength = 2.0f;
 
+    AudioSource audioSource = null;
+
+    private void OnStart()
+    {
+        audioSource = this.transform.GetComponent<AudioSource>();
+    }
+
     public void OnTriggerEnter(Collider entity)
     {
         if (entity.tag == "Character")
         {
             entity.GetComponentInParent<PlayerController>()?.HitDeathZone();
             Camera.main.transform.DOShakePosition(cameraShakeDuration, cameraShakeStrength);
+
+            // Try again to find death zone audio source
+            if(audioSource == null) audioSource = this.transform.GetComponent<AudioSource>();
+
+            if (audioSource != null)
+            {
+                audioSource.Play();
+                Debug.Log("Playing death zone sound.");
+            }
         }
     }
 }
