9f9150ca7f43102bb206f76f961c31b388083ad0
datWeazel <kagayushi@hotmail.de>
2020-01-03 17:13:09 +0100

    Added Camera Shake on Player Death

--- PDM Charlie/Assets/Scripts/GameLogic/DeathZonesController.cs
+++ PDM Charlie/Assets/Scripts/GameLogic/DeathZonesController.cs
@@ -1,14 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using DG.Tweening;
 using UnityEngine;
+
 
 public class DeathZonesController : MonoBehaviour
 {
+    const float cameraShakeDuration = 0.5f;
+    const float cameraShakeStrength = 2.0f;
+
     public void OnTriggerEnter(Collider entity)
     {
         if (entity.tag == "Character")
         {
             entity.GetComponentInParent<PlayerController>()?.HitDeathZone();
+            Camera.main.transform.DOShakePosition(cameraShakeDuration, cameraShakeStrength);
         }
     }
 }
