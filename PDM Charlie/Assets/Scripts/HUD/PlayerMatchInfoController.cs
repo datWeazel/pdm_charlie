176aa2895d327dbbb0fb5162cf34603c7aed4c82
datWeazel <kagayushi@hotmail.de>
2020-01-05 18:10:46 +0100

    Added UI shake effect when getting percentage damage

--- PDM Charlie/Assets/Scripts/HUD/PlayerMatchInfoController.cs
+++ PDM Charlie/Assets/Scripts/HUD/PlayerMatchInfoController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
@@ -10,6 +11,9 @@ public class PlayerMatchInfoController : MonoBehaviour
     public TextMeshProUGUI playerPercentage;
     public TextMeshProUGUI playerStocks;
 
+    private float shakeDuration = 1.0f;
+    private float shakeStrength = 2.5f;
+
     public void UpdatePlayerName(string newName)
     {
         this.playerName.text = newName;   
@@ -18,6 +22,7 @@ public class PlayerMatchInfoController : MonoBehaviour
     public void UpdatePlayerPercentage(float newPercentage)
     {
         this.playerPercentage.text = $"{(Math.Round(newPercentage, 1))}%";
+        this.playerPercentage.transform.DOShakePosition(shakeDuration, shakeStrength);
     }
 
     public void UpdatePlayerPercentage(string newText)
